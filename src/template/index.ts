679b5fa69b01ebb8ea1f290e71feb918a4d36ace
Daniel Corcoran <danielc.92@outlook.com>
2024-01-04 15:52:40 +1100

    feat: stop recursing after one level

--- src/template/index.ts
+++ src/template/index.ts
@@ -11,19 +11,47 @@ type TemplateBuilderConfig = {
 class TemplateBuilder {
   
   config: TemplateBuilderConfig
+  fieldTracker: Map<string, number>
 
   constructor(config:TemplateBuilderConfig) {
     this.config = config
+    this.fieldTracker = new Map<string, number>()
   }
 
   // get the core type of a graphql type eg '[String!]!' => 'String'
   private _cleanType = (type:string ) => type.replace(/[\W_]+/g,"")
 
-  private _getFieldNamesRecurs = (rawType: string, graphSchema: GraphQLSchema): string => {
+  private _getFieldNamesRecurs = (rawType: string, graphSchema: GraphQLSchema, parentFieldKey?: string): string => {
     const parsedType =  this._cleanType(rawType)
+
     if (graphSchema.getTypeMap()[parsedType] instanceof GraphQLObjectType) {
       const fields = getFieldsFromRoot(graphSchema, parsedType)
-      return `{${fields.map(f => `${f.name} ${this._getFieldNamesRecurs(f.type.toString(), graphSchema)}`).join(" ")}}`
+      for (const field of fields) {
+       
+        const fieldKey = `${field.name}__${field.type}`
+        if (this.fieldTracker.has(fieldKey)) {
+          // increment  visit
+          this.fieldTracker.set(fieldKey, this.fieldTracker.get(fieldKey)! + 1)
+        } else {
+          // the first visit
+          this.fieldTracker.set(fieldKey, 1)
+        }
+       
+      }
+      return `{${fields
+        .filter(
+          field => {
+            const fieldKey = `${field.name}__${field.type}`
+            const currentVisit = this.fieldTracker.get(fieldKey)
+            const isRecursiveField = fieldKey === parentFieldKey
+            if (typeof currentVisit === "number" && currentVisit >= 2 && isRecursiveField) {
+              return false
+            }
+
+            return true
+          }
+        )
+        .map(f => `${f.name} ${this._getFieldNamesRecurs(f.type.toString(), graphSchema, `${f.name}__${f.type}`)}`).join(" ")}}`
     } 
     return ""
   }
